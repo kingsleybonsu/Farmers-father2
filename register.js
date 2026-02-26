a744789d851e9a0f4026fb54e13c3c03bff12bd9
bonsukingsley <bonsukingsley90@gmail.com>
2026-02-26 21:30:08 +0000

    i have added some new files made some changes to some codes

---
+++ register.js
@@ -0,0 +1,21 @@
+// alert("Your registration has been completed successfully!  now continue with your payment to complete the process.");
+
+// console.log(10 +10);
+// console.log("hello world");
+
+
+// let username;
+// username = "Tom"
+// username = "king"
+// username = "kin"
+// username = "kim"
+// console.log(username)
+// username = "king"
+// console.log(username)
+
+
+
+//const
+// const name ="kigh"
+// let email = "kigh@gmail.com"
+// console.log(email)
