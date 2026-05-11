7c31b791a862c928e5a76c47914f6f7518e5b09c
bonsukingsley <bonsukingsley90@gmail.com>
2026-05-11 04:11:17 +0000

    made new changes

--- index.js
+++ index.js
@@ -6,17 +6,20 @@
 //   });
 
 /*//////massaging box/*/
-const feedbackForm = document.getElementById("contact-Us");
+/* FEEDBACK FORM */
+
+const feedbackForm =
+document.getElementById("contact-Us");
 
 feedbackForm.addEventListener("submit", function(e){
 
   e.preventDefault();
 
-  const email = document.getElementById("email").value;
+  const email =
+  document.getElementById("email").value;
 
-  const message = document.getElementById("massage").value;
-
-  // SAVE FEEDBACK
+  const message =
+  document.getElementById("massage").value;
 
   const feedbacks =
   JSON.parse(localStorage.getItem("feedbacks")) || [];
@@ -32,8 +35,34 @@ feedbackForm.addEventListener("submit", function(e){
     JSON.stringify(feedbacks)
   );
 
-  alert("Thank you for your feedback ❤️");
+  alert("Thank you for your feedback");
 
   feedbackForm.reset();
 
+});
+
+
+/* SIDEBAR */
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("closeBtn");
+const overlay = document.getElementById("overlay");
+
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.add("active");
+  overlay.classList.add("active");
+});
+
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
 });
