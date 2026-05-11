9f026377cdac3c47a02c86832d8728d10c45b5ef
bonsukingsley <bonsukingsley90@gmail.com>
2026-05-11 04:09:03 +0000

    made some new changes

--- index.js
+++ index.js
@@ -6,41 +6,34 @@
 //   });
 
 /*//////massaging box/*/
+/* FEEDBACK FOR
+ 
+/*/ /////massaging box/*/
 /* FEEDBACK FORM */
 
-const feedbackForm =
-document.getElementById("contact-Us");
+const feedbackForm = document.getElementById("contact-Us");
 
-feedbackForm.addEventListener("submit", function(e){
-
+feedbackForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const email =
-  document.getElementById("email").value;
+  const email = document.getElementById("email").value;
 
-  const message =
-  document.getElementById("massage").value;
+  const message = document.getElementById("massage").value;
 
-  const feedbacks =
-  JSON.parse(localStorage.getItem("feedbacks")) || [];
+  const feedbacks = JSON.parse(localStorage.getItem("feedbacks")) || [];
 
   feedbacks.push({
     email,
     message,
-    date: new Date().toLocaleString()
+    date: new Date().toLocaleString(),
   });
 
-  localStorage.setItem(
-    "feedbacks",
-    JSON.stringify(feedbacks)
-  );
+  localStorage.setItem("feedbacks", JSON.stringify(feedbacks));
 
   alert("Thank you for your feedback");
 
   feedbackForm.reset();
-
 });
-
 
 /* SIDEBAR */
 
@@ -49,18 +42,15 @@ const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("closeBtn");
 const overlay = document.getElementById("overlay");
 
-
 menuBtn.addEventListener("click", () => {
   sidebar.classList.add("active");
   overlay.classList.add("active");
 });
 
-
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
   overlay.classList.remove("active");
 });
-
 
 overlay.addEventListener("click", () => {
   sidebar.classList.remove("active");
